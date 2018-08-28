42836fdd2342c3417b50d1ff52623060e1f9e00c
svgds02 <dino.sardelic@svgroup.hr>
2018-08-28 17:41:02 +0200

    Changed from Absolute layout to a combination of standard layouts.

--- src/mvp/View.java
+++ src/mvp/View.java
@@ -1,6 +1,10 @@
 package mvp;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,6 +15,9 @@ import java.util.List;
 import java.util.Random;
 
 import javax.swing.AbstractButton;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -19,6 +26,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
 
 import properties.Messages;
 
@@ -39,10 +47,13 @@ public class View {
 	private JComboBox<String> cmTranslationDirection;
 	private JButton btnLoadNewDictionaryElement;
 	private JButton btnSelectDictionary;
+	private JPanel topPanel;
+	private JPanel leftPanel;
+	private JPanel rightPanel;
+	private JPanel articlesPanel;
+	private JLabel lblGerWord;
 
 	private Presenter presenter;
-
-	private static final Color REGULAR_GRAY = new Color(240, 240, 240);
 
 	public View() {
 		initialize();
@@ -59,67 +70,25 @@ public class View {
 	}
 
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 565, 350);
-		frame.setTitle("GERDict");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-
-		JLabel lblGerWord = new JLabel(Messages.getMessage(this.getClass(), "lblGerWord"));
+		
+		// TOP PANEL
+		
+		topPanel = new JPanel();
+		topPanel.setLayout(new FlowLayout());
+		
+		lblGerWord = new JLabel(Messages.getMessage(this.getClass(), "lblGerWord"));
 		lblGerWord.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblGerWord.setBounds(26, 23, 73, 25);
-		frame.getContentPane().add(lblGerWord);
-
+		topPanel.add(lblGerWord);
+		
 		tfUntranslatedWord = new JTextField();
 		tfUntranslatedWord.setEnabled(false);
 		tfUntranslatedWord.setFont(new Font("Tahoma", Font.BOLD, 20));
-		tfUntranslatedWord.setBounds(98, 23, 322, 25);
-		tfUntranslatedWord.setBackground(REGULAR_GRAY);
-		frame.getContentPane().add(tfUntranslatedWord);
+		tfUntranslatedWord.setBackground(new Color(240, 240, 240));
 		tfUntranslatedWord.setColumns(20);
-
-		JLabel lblArticle = new JLabel(Messages.getMessage(this.getClass(), "lblArticle"));
-		lblArticle.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblArticle.setBounds(63, 69, 56, 16);
-		frame.getContentPane().add(lblArticle);
-
-		JLabel lblTranslation = new JLabel(Messages.getMessage(this.getClass(), "lblTranslation"));
-		lblTranslation.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblTranslation.setBounds(320, 69, 83, 16);
-		frame.getContentPane().add(lblTranslation);
-
-		JPanel pnlArticles = new JPanel();
-		pnlArticles.setBounds(36, 98, 83, 104);
-		frame.getContentPane().add(pnlArticles);
-		pnlArticles.setLayout(null);
-
-		rdbtnGerArticleDer = new JRadioButton(Model.DER);
-		rdbtnGerArticleDer.setEnabled(false);
-		rdbtnGerArticleDer.setFont(new Font("Tahoma", Font.BOLD, 18));
-		rdbtnGerArticleDer.setBounds(15, 5, 60, 25);
-		rdbtnGerArticleDer.setActionCommand(Model.DER);
-		rdbtnGerArticleDer.addActionListener(new MyRDBTGerArticleActionListener());
-		pnlArticles.add(rdbtnGerArticleDer);
-
-		rdbtnGerArticleDie = new JRadioButton(Model.DIE);
-		rdbtnGerArticleDie.setEnabled(false);
-		rdbtnGerArticleDie.setFont(new Font("Tahoma", Font.BOLD, 18));
-		rdbtnGerArticleDie.setBounds(15, 35, 60, 25);
-		rdbtnGerArticleDie.setActionCommand(Model.DIE);
-		rdbtnGerArticleDie.addActionListener(new MyRDBTGerArticleActionListener());
-		pnlArticles.add(rdbtnGerArticleDie);
-
-		rdbtnGerArticleDas = new JRadioButton(Model.DAS);
-		rdbtnGerArticleDas.setEnabled(false);
-		rdbtnGerArticleDas.setFont(new Font("Tahoma", Font.BOLD, 18));
-		rdbtnGerArticleDas.setBounds(15, 65, 60, 25);
-		rdbtnGerArticleDas.setActionCommand(Model.DAS);
-		rdbtnGerArticleDas.addActionListener(new MyRDBTGerArticleActionListener());
-		pnlArticles.add(rdbtnGerArticleDas);
-
+		topPanel.add(tfUntranslatedWord);
+		
 		btnLoadNewDictionaryElement = new JButton(Messages.getMessage(this.getClass(), "btnLoadNewDictionaryElement"));
 		btnLoadNewDictionaryElement.setEnabled(false);
-		btnLoadNewDictionaryElement.setBounds(432, 24, 97, 25);
 		btnLoadNewDictionaryElement.setMnemonic(KeyEvent.VK_ENTER);
 		btnLoadNewDictionaryElement.addActionListener(new ActionListener() {
 			@Override
@@ -127,65 +96,143 @@ public class View {
 				btnLoadNewDictionaryElementActionPerformed();
 			}
 		});
-		frame.getContentPane().add(btnLoadNewDictionaryElement);
-
-		JPanel pnlTranslationSelection = new JPanel();
-		pnlTranslationSelection.setBounds(182, 82, 367, 213);
-		frame.getContentPane().add(pnlTranslationSelection);
-		pnlTranslationSelection.setLayout(null);
-
-		rdbtnTranslation1 = new JRadioButton("");
-		rdbtnTranslation1.setEnabled(false);
-		rdbtnTranslation1.setFont(new Font("Tahoma", Font.BOLD, 18));
-		rdbtnTranslation1.setBounds(8, 19, 351, 35);
-		rdbtnTranslation1.addActionListener(new MyRDBTTranslationActionListener());
-		pnlTranslationSelection.add(rdbtnTranslation1);
-
-		rdbtnTranslation2 = new JRadioButton("");
-		rdbtnTranslation2.setEnabled(false);
-		rdbtnTranslation2.setFont(new Font("Tahoma", Font.BOLD, 18));
-		rdbtnTranslation2.setBounds(8, 57, 351, 35);
-		rdbtnTranslation2.addActionListener(new MyRDBTTranslationActionListener());
-		pnlTranslationSelection.add(rdbtnTranslation2);
-
-		rdbtnTranslation3 = new JRadioButton("");
-		rdbtnTranslation3.setEnabled(false);
-		rdbtnTranslation3.setFont(new Font("Tahoma", Font.BOLD, 18));
-		rdbtnTranslation3.setBounds(8, 95, 351, 35);
-		rdbtnTranslation3.addActionListener(new MyRDBTTranslationActionListener());
-		pnlTranslationSelection.add(rdbtnTranslation3);
-
-		rdbtnTranslation4 = new JRadioButton("");
-		rdbtnTranslation4.setEnabled(false);
-		rdbtnTranslation4.setFont(new Font("Tahoma", Font.BOLD, 18));
-		rdbtnTranslation4.setBounds(8, 133, 351, 35);
-		rdbtnTranslation4.addActionListener(new MyRDBTTranslationActionListener());
-		pnlTranslationSelection.add(rdbtnTranslation4);
-
-		rdbtnTranslation5 = new JRadioButton("");
-		rdbtnTranslation5.setEnabled(false);
-		rdbtnTranslation5.setFont(new Font("Tahoma", Font.BOLD, 18));
-		rdbtnTranslation5.setBounds(8, 171, 351, 35);
-		rdbtnTranslation5.addActionListener(new MyRDBTTranslationActionListener());
-		pnlTranslationSelection.add(rdbtnTranslation5);
+		topPanel.add(btnLoadNewDictionaryElement);
+		
+		// LEFT PANEL
+		
+		leftPanel = new JPanel();
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+		TitledBorder leftPanelBorder = new TitledBorder(
+				BorderFactory.createEmptyBorder(),
+				Messages.getMessage(this.getClass(), "lblArticle"),
+				TitledBorder.CENTER,
+				TitledBorder.BELOW_TOP);
+		leftPanelBorder.setTitleFont(new Font("Tahoma", Font.PLAIN, 16));
+		leftPanel.setBorder(leftPanelBorder);
+		
+		leftPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+		
+		articlesPanel = new JPanel();
+		articlesPanel.setLayout(new BoxLayout(articlesPanel, BoxLayout.PAGE_AXIS));
+		articlesPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		leftPanel.add(articlesPanel);
+		
+		rdbtnGerArticleDer = new JRadioButton(Model.DER);
+		rdbtnGerArticleDer.setEnabled(false);
+		rdbtnGerArticleDer.setFont(new Font("Tahoma", Font.BOLD, 18));
+		rdbtnGerArticleDer.setActionCommand(Model.DER);
+		rdbtnGerArticleDer.addActionListener(new MyRDBTGerArticleActionListener());
+		rdbtnGerArticleDer.setAlignmentX(Component.LEFT_ALIGNMENT);
+		articlesPanel.add(rdbtnGerArticleDer);
+		
+		articlesPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+		
+		rdbtnGerArticleDie = new JRadioButton(Model.DIE);
+		rdbtnGerArticleDie.setEnabled(false);
+		rdbtnGerArticleDie.setFont(new Font("Tahoma", Font.BOLD, 18));
+		rdbtnGerArticleDie.setActionCommand(Model.DIE);
+		rdbtnGerArticleDie.addActionListener(new MyRDBTGerArticleActionListener());
+		rdbtnGerArticleDie.setAlignmentX(Component.LEFT_ALIGNMENT);
+		articlesPanel.add(rdbtnGerArticleDie);
+		
+		articlesPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+		
+		rdbtnGerArticleDas = new JRadioButton(Model.DAS);
+		rdbtnGerArticleDas.setEnabled(false);
+		rdbtnGerArticleDas.setFont(new Font("Tahoma", Font.BOLD, 18));
+		rdbtnGerArticleDas.setActionCommand(Model.DAS);
+		rdbtnGerArticleDas.addActionListener(new MyRDBTGerArticleActionListener());
+		rdbtnGerArticleDas.setAlignmentX(Component.LEFT_ALIGNMENT);
+		articlesPanel.add(rdbtnGerArticleDas);
+		
+		leftPanel.add(Box.createRigidArea(new Dimension(0, 15)));
+		leftPanel.add(Box.createVerticalGlue());
 
 		btnSelectDictionary = new JButton(Messages.getMessage(this.getClass(), "btnSelectDictionary"));
-		btnSelectDictionary.setBounds(12, 211, 139, 49);
 		btnSelectDictionary.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				btnSelectDictionaryActionPerformed();
 			}
 		});
-		frame.getContentPane().add(btnSelectDictionary);
+		btnSelectDictionary.setAlignmentX(Component.CENTER_ALIGNMENT);
+		leftPanel.add(btnSelectDictionary);
+		
+		leftPanel.add(Box.createRigidArea(new Dimension(0, 5)));
 
 		cmTranslationDirection = new JComboBox<String>();
 		cmTranslationDirection.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		cmTranslationDirection.setBounds(12, 273, 139, 32);
 		cmTranslationDirection.addItem(Model.FROM_GERMAN);
 		cmTranslationDirection.addItem(Model.TO_GERMAN);
 		cmTranslationDirection.setSelectedItem(Model.FROM_GERMAN);
-		frame.getContentPane().add(cmTranslationDirection);
+		cmTranslationDirection.setAlignmentX(Component.CENTER_ALIGNMENT);
+		cmTranslationDirection.setMaximumSize(new Dimension(Integer.MAX_VALUE, cmTranslationDirection.getMinimumSize().height));
+		leftPanel.add(cmTranslationDirection);
+		
+		// RIGHT PANEL
+		
+		rightPanel = new JPanel();
+		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.PAGE_AXIS));
+		TitledBorder rightPanelBorder = new TitledBorder(
+				BorderFactory.createEmptyBorder(),
+				Messages.getMessage(this.getClass(), "lblTranslation"),
+				TitledBorder.CENTER,
+				TitledBorder.BELOW_TOP);
+		rightPanelBorder.setTitleFont(new Font("Tahoma", Font.PLAIN, 16));
+		rightPanel.setBorder(rightPanelBorder);
+		
+		rightPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+		
+		rdbtnTranslation1 = new JRadioButton(" ");
+		rdbtnTranslation1.setEnabled(false);
+		rdbtnTranslation1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		rdbtnTranslation1.addActionListener(new MyRDBTTranslationActionListener());
+		rightPanel.add(rdbtnTranslation1);
+		
+		rightPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+		rdbtnTranslation2 = new JRadioButton(" ");
+		rdbtnTranslation2.setEnabled(false);
+		rdbtnTranslation2.setFont(new Font("Tahoma", Font.BOLD, 18));
+		rdbtnTranslation2.addActionListener(new MyRDBTTranslationActionListener());
+		rightPanel.add(rdbtnTranslation2);
+		
+		rightPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+		rdbtnTranslation3 = new JRadioButton(" ");
+		rdbtnTranslation3.setEnabled(false);
+		rdbtnTranslation3.setFont(new Font("Tahoma", Font.BOLD, 18));
+		rdbtnTranslation3.addActionListener(new MyRDBTTranslationActionListener());
+		rightPanel.add(rdbtnTranslation3);
+		
+		rightPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+		rdbtnTranslation4 = new JRadioButton(" ");
+		rdbtnTranslation4.setEnabled(false);
+		rdbtnTranslation4.setFont(new Font("Tahoma", Font.BOLD, 18));
+		rdbtnTranslation4.addActionListener(new MyRDBTTranslationActionListener());
+		rightPanel.add(rdbtnTranslation4);
+		
+		rightPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
+		rdbtnTranslation5 = new JRadioButton(" ");
+		rdbtnTranslation5.setEnabled(false);
+		rdbtnTranslation5.setFont(new Font("Tahoma", Font.BOLD, 18));
+		rdbtnTranslation5.addActionListener(new MyRDBTTranslationActionListener());
+		rightPanel.add(rdbtnTranslation5);
+		
+		rightPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+		
+		// FRAME
+		
+		frame = new JFrame();
+		frame.setTitle("GERDict");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout(30, 10));
+		frame.getContentPane().add(topPanel, BorderLayout.PAGE_START);
+		frame.getContentPane().add(leftPanel, BorderLayout.LINE_START);
+		frame.getContentPane().add(rightPanel, BorderLayout.CENTER);
+		frame.pack();
 	}
 
 	public void setVisible(boolean isVisible) {
@@ -213,7 +260,7 @@ public class View {
 		tfUntranslatedWord.setText(getPresenter().getModel().getUntranslatedWord());
 		for (AbstractButton abstractButton : Collections.list(btngrpGerArticles.getElements())) {
 			JRadioButton rdbtnArticle = (JRadioButton) abstractButton;
-			rdbtnArticle.setBackground(REGULAR_GRAY);
+			rdbtnArticle.setBackground(new Color(240, 240, 240));
 			if (getPresenter().getModel().getCorrectGerArticle() == null) {
 				rdbtnArticle.setEnabled(false);
 			} else {
@@ -227,7 +274,7 @@ public class View {
 		for (AbstractButton abstractButton: Collections.list(btngrpTranslations.getElements())) {
 			JRadioButton rdbtnTranslation = (JRadioButton) abstractButton;
 			rdbtnTranslation.setEnabled(true);
-			rdbtnTranslation.setBackground(REGULAR_GRAY);
+			rdbtnTranslation.setBackground(new Color(240, 240, 240));
 			rdbtnTranslation.setText(possibleTranslations.get(i));
 			rdbtnTranslation.setActionCommand(possibleTranslations.get(i));
 			i++;
@@ -245,7 +292,7 @@ public class View {
 				if (rdbtnAnswer.isSelected()) {
 					rdbtnAnswer.setBackground(Color.RED);
 				} else {
-					rdbtnAnswer.setBackground(REGULAR_GRAY);
+					rdbtnAnswer.setBackground(new Color(240, 240, 240));
 				}
 			}
 		}
@@ -260,7 +307,7 @@ public class View {
 				if (rdbtnTranslation.isSelected()) {
 					rdbtnTranslation.setBackground(Color.RED);
 				} else {
-					rdbtnTranslation.setBackground(REGULAR_GRAY);
+					rdbtnTranslation.setBackground(new Color(240, 240, 240));
 				}
 			}
 		}
